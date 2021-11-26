2ff7ec15054e97169e6459b41d5e92d68114ba7a
fernandozoe <fernandozoe180@yahoo.com>
2021-11-26 17:37:20 -0300

    .

---
+++ src/main/java/com/aws/lambda/notification/AwsLambdaNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.aws.lambda.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLambdaNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/lambda/notification/AwsLambdaNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.lambda.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLambdaNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
